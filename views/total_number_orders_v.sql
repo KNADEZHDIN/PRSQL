2c3bd9f0799c1a59140fb22637ab03b133407c8a
Alex Stupak <itskills4practice@gmail.com>
2023-09-04 17:37:43 +0300

    PRSQL1-27: fix select query

--- views/total_number_orders_v.sql
+++ views/total_number_orders_v.sql
@@ -1,2 +1,12 @@
 create view total_number_orders_v as
-select * from orders;
+select o.status_id, 
+      count(o.order_id) as number_of_order, 
+      s.description
+from orders o
+join status s
+on o.status_id = s.status_id
+group by o.status_id, s.description;
+
+
+
+
